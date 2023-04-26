a2cf02498067e3acc81b78357f914188fc0983c5
Shamil Kurbanov <107269680+KShamil@users.noreply.github.com>
2023-04-26 18:20:52 +0400

    update env

--- src/services/services.ts
+++ src/services/services.ts
@@ -9,42 +9,42 @@ import { OperationsData } from '@/interfaces/interfaces';
 
 
 export async function getData(): Promise<CardData[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_TRANSITIONS}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/transitions`);
     return await response.json()
 }
 
 export async function getOffersCardData(): Promise<OffersCardData[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_OFFERS}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/offers`);
     return await response.json()
 }
 
 
 export async function getCorporativeData(): Promise<CorporativeData[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_CORPORATE}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/corporative`);
     return await response.json()
 }
 
 export async function getBusinessCardData(): Promise<BusinessCardData[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BUSINESS}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/business`);
     return await response.json()
 }
 
 export async function getCorporateSliderItemsData(): Promise<CorporateSliderData[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_CORPORATESLIDER}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/corporatesliderdata`);
     return await response.json()
 }
 
 export async function getKobSliderItemsData(): Promise<KobSliderData[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_KOBSLIDERDATA}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/kobsliderdata`);
     return await response.json()
 }
 
 export async function getPersonalSliderItemsData(): Promise<PersonalSliderData[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_PERSONALSLIDERDATA}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/personalsliderdata`);
     return await response.json()
 }
 
 export async function getOperationsData(): Promise<OperationsData[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_OPERATIONS}`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/operations`);
     return await response.json()
 }
